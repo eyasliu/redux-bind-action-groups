15bdcadb37a49d5883ec35968dfb777ede2b3375
eyas <eyas_liu@fmces.com>
2016-05-12 16:08:56 +0800

    group actions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export default {
-  
+import bindActionCreators from 'redux/lib/bindActionCreators';
+
+export default function bindActionGroups(actionGroups, dispatch){
+  if(typeof actionGroups !== 'object' || actionGroups === null){
+    throw new Error('bindActionGroups expected an object, key is group name, value is object with actionCreators');
+  }
+
+  var groupKeys = Object.keys(actionGroups);
+  var groupActionCreators = {};
+
+  for(var i = 0; i < groupKeys.length; i++){
+    var key = keys[i]
+    var actionCreator = actionGroups[key]
+    if (typeof actionCreator === 'function') {
+      groupActionCreators[key] = bindActionCreators(actionCreator, dispatch)
+    }
+  }
+
+  return groupActionCreators;
 }
